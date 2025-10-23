97a2c4db370b4affdfe2becd8b78a7571a6f29e8
Sachin Joshi <14989223+sachudj@users.noreply.github.com>
2025-10-23 21:08:02 +0530

    extra solutions seen

--- 3461 - Check If Digits Are Equal in String After Operations I/solution.py
+++ 3461 - Check If Digits Are Equal in String After Operations I/solution.py
@@ -6,4 +6,19 @@ class Solution:
                 new_s.append(str((int(s[i]) + int(s[i+1])) % 10))
             s = ''.join(new_s)
         return s[0] == s[1]
-        
+
+# Two other solutions I found while checking the solutions
+# Code #1
+# Time complexity: O(n^2). Space complexity: O(n).
+#
+# class Solution:
+#     def hasSameDigits(self, s: str) -> bool:
+#         return eq(*reduce(lambda q,_:[sum(p)%10 for p in pairwise(q)],s[2:],map(int,s)))
+#
+#
+# Code #2
+# Time complexity: O(n^2). Space complexity: O(n).
+#
+# class Solution:
+#     def hasSameDigits(self, s: str) -> bool:
+#         return (f:=lambda q:f([sum(p)%10 for p in pairwise(q)]) if len(q)>2 else eq(*q))([*map(int,s)])
